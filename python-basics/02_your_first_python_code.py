3f8b7e63ccfff37ac73007f6c3a08c10181ae0e7
Renata Oliveira <47616126+Miyu7x@users.noreply.github.com>
2026-05-14 13:01:27 -0400

    Update 02_your_first_python_code.py

--- python-basics/02_your_first_python_code.py
+++ python-basics/02_your_first_python_code.py
@@ -1,13 +1,18 @@
 # Course: Introduction to Python
 # Chapter 1: Python Basics
-# Exercise: Your First Python Code
+# Exercise: Hello Python
 
 # -------------------------------------------------------
 # Notes
+# print() displays output to the console
+# python executes code from top to bottom 
+# / divides and retuns a decimal (float)
 # -------------------------------------------------------
 #
 
 # -------------------------------------------------------
-# Solution
+# Solution 
+# Hit run code to see the output!
+print(5 / 8)
 # -------------------------------------------------------
 
