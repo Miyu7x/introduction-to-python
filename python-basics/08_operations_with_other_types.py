97260190859bf421d299b0ceea741bedd32c02a1
Renata Oliveira <47616126+Miyu7x@users.noreply.github.com>
2026-05-14 14:21:27 -0400

    Update 08_operations_with_other_types.py

--- python-basics/08_operations_with_other_types.py
+++ python-basics/08_operations_with_other_types.py
@@ -26,3 +26,13 @@ print(total_savings)
 
 # Print the type of total_savings
 print(type(total_savings))
+
+# 2
+
+intro = "Hello! How are you?"
+
+# Assign sum of intro and intro to doubleintro
+doubleintro= (intro + intro)
+
+# Print out doubleintro
+print(doubleintro)
