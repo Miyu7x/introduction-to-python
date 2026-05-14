6a985d8e1a253aa370c6ad79f3840cafc0477a0c
Renata Oliveira <47616126+Miyu7x@users.noreply.github.com>
2026-05-14 14:13:34 -0400

    Update 08_operations_with_other_types.py

--- python-basics/08_operations_with_other_types.py
+++ python-basics/08_operations_with_other_types.py
@@ -5,9 +5,24 @@
 # -------------------------------------------------------
 # Notes
 # -------------------------------------------------------
-#
+
+# + on two ints or floats does math: 2 + 3 = 5
+
+# + on two strings glues them together: "Hello" + "Renata" = "HelloRenata" (called concatenation)
+
+# * on a string and a number repeats it: "ha" * 3 = "hahaha"
+
+# + on two booleans treats True as 1 and False as 0: True + True = 2
 
 # -------------------------------------------------------
 # Solution
 # -------------------------------------------------------
+savings = 100
+new_savings = 40
 
+# Calculate total_savings using savings and new_savings
+total_savings = (savings + new_savings)
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
