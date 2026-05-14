0bf0aebd7fd35abb233ef4fad9171ec85c3f7007
Renata Oliveira <47616126+Miyu7x@users.noreply.github.com>
2026-05-14 13:44:30 -0400

    Update 06_calculations_with_variables.py

--- python-basics/06_calculations_with_variables.py
+++ python-basics/06_calculations_with_variables.py
@@ -4,10 +4,16 @@
 
 # -------------------------------------------------------
 # Notes
+# Python allows us to calcuclate with variables 
+# Storing results in a new variable keeps calculations readable
 # -------------------------------------------------------
 #
 
 # -------------------------------------------------------
 # Solution
 # -------------------------------------------------------
+monthly_savings = 10 # you saved $10 each month
+num_months = 4 # you saved money for 4 months
+new_savings = (monthly_savings * num_months) # multiple money saved for four months and set it to new_savings
+print(new_savings)
 
