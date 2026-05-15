7b6e812c7e7c27123d4fa4229f93c391b9e96e42
Renata Oliveira <47616126+Miyu7x@users.noreply.github.com>
2026-05-15 19:46:57 -0400

    completed create a list
    Added a list to group area values and print them.

--- python-lists/02_create_a_list.py
+++ python-lists/02_create_a_list.py
@@ -5,9 +5,23 @@
 # -------------------------------------------------------
 # Notes
 # -------------------------------------------------------
+# Lists are a compound data type, you can group values together as such:
+# a = is
+# b = cute
+# my_cat = ["my", "cat", a, b]
 #
-
+# quotes "" = strings, no matter the type, text, numbers, sentence
 # -------------------------------------------------------
 # Solution
 # -------------------------------------------------------
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
 
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
