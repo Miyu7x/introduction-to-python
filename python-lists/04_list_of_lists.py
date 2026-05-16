3347e1390c55194a25d339faa669ce8c6d6ff987
Renata Oliveira <47616126+Miyu7x@users.noreply.github.com>
2026-05-15 20:08:36 -0400

    Complete house list with bedroom and bathroom
    Added bedroom and bathroom data to the house list.

--- python-lists/04_list_of_lists.py
+++ python-lists/04_list_of_lists.py
@@ -5,9 +5,29 @@
 # -------------------------------------------------------
 # Notes
 # -------------------------------------------------------
-#
-
+# As an SOC we will be dealing with a lot of data, we need to group this data
+# Instead of a list with a mix of strings and floats, that represents names and numbers you create a list of lists
+# "hallway" string, hall is a variable that is set to a float 11.25
+'''
+Instructions
+Finish the list of lists so that it also contains the bedroom and bathroom data. Make sure you enter these in order!
+Print out house; does this way of structuring your data make more sense?
+'''
 # -------------------------------------------------------
 # Solution
 # -------------------------------------------------------
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
 
+# House information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+        ["bedroom", bed],
+        ["bathroom", bath]]
+
+# Print out house
+print(house)
